0d8797080bbc0bd845ed0cb8724e52140c720ab8
ismahen <https://github.com/Ismahen/DataMapper>
2015-10-15 21:54:53 +0100

    Data Mapper class

---
+++ DataMapper/DataMapper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public static class DataMapper
+    {
+        public static T ConvertToEntity<T>(this DataRow tableRow) where T : new()
+        {
+            // Create a new type of the entity I want
+            Type t = typeof(T);
+            T returnObject = new T();
+
+            foreach (DataColumn col in tableRow.Table.Columns)
+            {
+                string colName = col.ColumnName;
+
+                // Look for the object's property with the columns name, ignore case
+                PropertyInfo pInfo = t.GetProperty(colName.ToLower().Replace("_", ""),
+                    BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                // did we find the property ?
+                if (pInfo != null)
+                {
+                    object val = tableRow[colName];
+
+                    // is this a Nullable<> type
+                    bool IsNullable = (Nullable.GetUnderlyingType(pInfo.PropertyType) != null);
+                    if (IsNullable)
+                    {
+                        if (val is System.DBNull)
+                        {
+                            val = null;
+                        }
+                        else
+                        {
+                            // Convert the db type into the T we have in our Nullable<T> type
+                            val = Convert.ChangeType
+                    (val, Nullable.GetUnderlyingType(pInfo.PropertyType));
+                        }
+                    }
+                    else
+                    {
+                        if (val is System.DBNull == false)
+                            // Convert the db type into the type of the property in our entity
+                            val = Convert.ChangeType(val, pInfo.PropertyType);
+                    }
+                    // Set the value of the property with the value from the db
+
+                    if (val is System.DBNull == false)
+                        pInfo.SetValue(returnObject, val, null);
+                }
+            }
+
+            // return the entity object with values
+            return returnObject;
+        }
+
+        public static List<T> ConvertToList<T>(this DataTable table) where T : new()
+        {
+            Type t = typeof(T);
+
+            // Create a list of the entities we want to return
+            List<T> returnObject = new List<T>();
+
+            // Iterate through the DataTable's rows
+            foreach (DataRow dr in table.Rows)
+            {
+                // Convert each row into an entity object and add to the list
+                T newRow = dr.ConvertToEntity<T>();
+                returnObject.Add(newRow);
+            }
+
+            // Return the finished list
+            return returnObject;
+        }
+    }
+}
